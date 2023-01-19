2960e140cf4fa35e7daf2cb7ed4a8a6694a79038
Deepak Sharma <sharma.deepak8616@gmail.com>
2023-01-18 21:00:40 -0500

    Class3 code

---
+++ Modules/Class3/Operators.js
@@ -0,0 +1,13 @@
+/**
+ * Operators in JS:
+ * 
+ * 1. Assignment (=)
+ * 2. Arithmetic (+, -, *, /, %, ++, --)
+ * 3. Comparison (===, >=, >, <=, <, !==)
+ * 4. Logical (&&, ||, !)
+ * 5. Ternary (?:)
+ * 6. Advanced Assignment (+=, -=, *=, /=, %=)
+ * 
+ */
+
+// 8:25PM
